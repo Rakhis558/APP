fd9e6e22938fd591e92cbfee07d80251e188e800
Rakhi Soni <100033123+Rakhis558@users.noreply.github.com>
2022-06-12 10:04:14 +0530

    Add files via upload

---
+++ Covid.App/js/myscript.js
@@ -0,0 +1,38 @@
+const URL="https://covid.mathdro.id/api";
+
+
+let app = angular.module("MyApp",[]);
+
+ app.controller("MyCtrl",($scope, $http)=> {
+    //this is controller
+    $scope.title = "Stay Home Stay Safe";
+
+    console.log("APP Loded");
+    //calling api
+
+    $http.get(URL).then((response)=>{
+        //success
+        console.log(response.data);
+
+        $scope.all_data=response.data;
+    } ,
+    (error)=>{
+        //error
+        console.log(error);
+    });
+    //get country data
+    $scope.get_c_data = () => {
+        let country =$scope.c;
+        if(country=="") {
+            $scope.c_data=undefined;
+            return;
+        }
+        $http.get('${URL}/countries/${country}')
+        .then((response) =>{
+           console.log(response.data);
+           $scope.c_data = response.data;
+        } , (error)=>{
+            console.log(error);
+        });
+    };
+});
